68fe7755989249dfa58f58724d2ce51c8c969bc4
Rayumar89 <rayumarj@gmail.com>
2023-09-25 07:11:05 -0700

    js1

---
+++ app.js
@@ -0,0 +1,5 @@
+alert ("Please enter a valid password")
+alert ("Well come to Js land.....\n Happy coding")
+alert ("Happy coding....\n Prevent this page from creating additional dialogs")
+console.log("Hallo... I can run JS through my web browser's console");
+document.write("Hallo... I can run JS through my web browser's console")
